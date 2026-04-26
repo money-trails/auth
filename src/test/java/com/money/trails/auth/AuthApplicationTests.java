811bce4e5123d77cbcedd79c4bb5543696aa1d38
Soumen <me.soumen02@gmail.com>
2026-04-27 01:00:18 +0530

    Initial check in

---
+++ src/test/java/com/money/trails/auth/AuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.money.trails.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
