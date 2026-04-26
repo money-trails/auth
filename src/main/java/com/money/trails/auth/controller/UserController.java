ba82a7cf943c9cc3ea5aeff703e23555a33944c8
Soumen <me.soumen02@gmail.com>
2026-04-27 02:21:51 +0530

    Fixed login endpoint

--- src/main/java/com/money/trails/auth/controller/UserController.java
+++ src/main/java/com/money/trails/auth/controller/UserController.java
@@ -6,22 +6,26 @@ import com.money.trails.auth.service.AuthService;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/v1/auth")
+@RequiredArgsConstructor
 @Tag(name = "UserController", description = "User login and register controller")
 public class UserController {
 
-    private AuthService authService;
+    private final AuthService authService;
 
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Successful login"),
+            @ApiResponse(responseCode = "200", description = "For successful login"),
             @ApiResponse(responseCode = "401", description = "Unauthorized access")
     })
+    @PostMapping
     public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request) {
         return ResponseEntity.ok().body(authService.login(request));
     }
